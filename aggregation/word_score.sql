5adce19964102b6f5b3c96c0e9a0c7350b0e28a7
Jae Il Lee <jeiaiel@gmail.com>
2017-12-14 21:40:47 -0800

    Update word_score.sql

--- aggregation/word_score.sql
+++ aggregation/word_score.sql
@@ -1,32 +1,87 @@
-CREATE TABLE review_compact 
+-- Create a new review table with minimum column
+CREATE TABLE 
+	review_compact 
 AS 
 SELECT 
+	user_id, 
 	text, 
-	stars
-FROM review 
-WHERE text IS NOT NULL
-AND text NOT IN ("1","2","3","4","5")
-AND stars IN ("1","2","3","4","5")
+	stars 
+FROM 
+	review 
+WHERE text IS NOT NULL 
+AND TEXT NOT IN ("1","2","3","4","5")
+AND stars IN ("1","2","3","4","5") 
+AND LENGTH(user_id) ==22
 ;
 
-CREATE TABLE review_words 
+
+-- Tokenize review text to words
+CREATE TABLE 
+	review_words_temp 
 AS 
 SELECT 
+	user_id, 
 	words, 
 	stars 
-from review_compact 
-LATERAL VIEW EXPLODE(SPLIT(text," ")) TEXTS AS WORDS
+FROM 
+	review_compact 
+LATERAL VIEW EXPLODE(SPLIT(text," ")) texts AS words
 ;
 
 
-CREATE TABLE review_words_avg
+-- Remove special character, lowercase all words
+CREATE TABLE 
+	review_words_temp_2 
 AS 
-SELECT  
-	words, 
-	AVG(stars) 
-	average_stars, 
-	COUNT(words) count 
-FROM review_words 
-GROUP BY words
+SELECT
+	user_id, 
+	LOWER(regexp_replace(translate(words,'.',''),"\!\?","")) AS words, 
+	stars 
+FROM 
+	review_words_temp
 ;
-	
+
+
+-- Create a new user table with minimum column
+CREATE TABLE 
+	user_compact 
+AS 
+SELECT 
+	user_id, 
+	average_stars 
+FROM 
+	user 
+WHERE LENGTH(user_id) ==22
+;
+
+
+
+-- Join Review table and User table on user_id and apply weight based on user's average stars
+CREATE TABLE 
+	review_words_weighted 
+AS 
+SELECT 
+	a.user_id, 
+	a.words AS word, 
+	a.stars stars, 
+	(a.stars - b.average_stars + 5) AS weighted_stars 
+FROM 
+	review_words_temp_2 a, 
+	user_compact b 
+WHERE a.user_id = b.user_id
+;
+
+
+-- Generate review_words table with the aggregated stars, weighted stars value
+CREATE TABLE 
+	review_words 
+AS 
+SELECT 
+	word, 
+	avg(stars) average_stars, 
+	avg(weighted_stars) AS weighted_average_stars, 
+	count(word) count 
+FROM 
+	review_words_weighted 
+GROUP BY word
+;
